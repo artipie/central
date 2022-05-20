fa97f9a2b356b53f86524a49acfc4d0ca6a3891f
Evgeny Chugunyy <chgena@mail.ru>
2022-05-20 20:43:18 +0800

    Added TKN_KEY variable for new artipie front

--- restart.sh
+++ restart.sh
@@ -4,6 +4,7 @@ export OAUTH_CLIENT="0f33052d9fee8303c36e"
 export OAUTH_SECRET="e57d898182a2f68d9a44c1c69a892a1b66e85be0"
 export SESSION_KEY="/var/artipie/keys/artipie.der"
 export ARTIPIE_SESSION_KEY="/var/artipie/keys/artipie-priv.der"
+export TKN_KEY="" #variable must be set for artipie/front for future GitHub API integration
 
 docker-compose build
 docker-compose pull artipie
